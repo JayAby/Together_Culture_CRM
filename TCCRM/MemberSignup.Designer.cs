2de20e19f7fd07329293f4526c5f1c2600454f62
Joel Olubankole <jay.aby.codes@gmail.com>
2025-05-23 15:36:20 +0100

    Added Member Signup.
    Format app to start with admin or member login. Focus on member, member should have choice to signup and login.

---
+++ TCCRM/MemberSignup.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace TCCRM
+{
+    partial class MemberSignup
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.ckbAgreement = new System.Windows.Forms.CheckBox();
+            this.btnSignup = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Candara", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtFirstName.Location = new System.Drawing.Point(117, 112);
+            this.txtFirstName.Multiline = true;
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(378, 46);
+            this.txtFirstName.TabIndex = 6;
+            this.txtFirstName.Text = "FirstName";
+            this.txtFirstName.Enter += new System.EventHandler(this.txtFirstName_Enter);
+            this.txtFirstName.Leave += new System.EventHandler(this.txtFirstName_Leave);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Candara", 10.8F);
+            this.txtLastName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtLastName.Location = new System.Drawing.Point(550, 112);
+            this.txtLastName.Multiline = true;
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(298, 46);
+            this.txtLastName.TabIndex = 7;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Candara", 10.8F);
+            this.txtEmail.Location = new System.Drawing.Point(117, 198);
+            this.txtEmail.Multiline = true;
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(378, 46);
+            this.txtEmail.TabIndex = 8;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("Candara", 10.8F);
+            this.txtPhone.Location = new System.Drawing.Point(550, 198);
+            this.txtPhone.Multiline = true;
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(298, 46);
+            this.txtPhone.TabIndex = 9;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Candara", 10.8F);
+            this.txtPassword.Location = new System.Drawing.Point(117, 284);
+            this.txtPassword.Multiline = true;
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(731, 46);
+            this.txtPassword.TabIndex = 10;
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Font = new System.Drawing.Font("Candara", 10.8F);
+            this.txtConfirmPassword.Location = new System.Drawing.Point(117, 369);
+            this.txtConfirmPassword.Multiline = true;
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(731, 46);
+            this.txtConfirmPassword.TabIndex = 11;
+            // 
+            // ckbAgreement
+            // 
+            this.ckbAgreement.AutoSize = true;
+            this.ckbAgreement.Font = new System.Drawing.Font("Candara", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ckbAgreement.Location = new System.Drawing.Point(148, 487);
+            this.ckbAgreement.Name = "ckbAgreement";
+            this.ckbAgreement.Size = new System.Drawing.Size(361, 22);
+            this.ckbAgreement.TabIndex = 12;
+            this.ckbAgreement.Text = "By ticking this, you Agree to our Terms and Conditions";
+            this.ckbAgreement.UseVisualStyleBackColor = true;
+            // 
+            // btnSignup
+            // 
+            this.btnSignup.Font = new System.Drawing.Font("Candara", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSignup.Location = new System.Drawing.Point(431, 546);
+            this.btnSignup.Name = "btnSignup";
+            this.btnSignup.Size = new System.Drawing.Size(177, 33);
+            this.btnSignup.TabIndex = 13;
+            this.btnSignup.Text = "Signup";
+            this.btnSignup.UseVisualStyleBackColor = true;
+            // 
+            // MemberSignup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(207)))), ((int)(((byte)(49)))), ((int)(((byte)(49)))));
+            this.Controls.Add(this.btnSignup);
+            this.Controls.Add(this.ckbAgreement);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtFirstName);
+            this.Name = "MemberSignup";
+            this.Size = new System.Drawing.Size(1043, 646);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+        private System.Windows.Forms.CheckBox ckbAgreement;
+        private System.Windows.Forms.Button btnSignup;
+    }
+}
---
+++ TCCRM/MemberSignup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCCRM
+{
+    public partial class MemberSignup : UserControl
+    {
+        public MemberSignup()
+        {
+            InitializeComponent();
+        }
+
+        private void txtFirstName_Enter(object sender, EventArgs e)
+        {
+            if (txtFirstName.Text == "FirstName")
+            {
+                txtFirstName.Text = "";
+                txtFirstName.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtFirstName_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFirstName.Text))
+            {
+                txtFirstName.Text = "FirstName";
+                txtFirstName.ForeColor= Color.Gray;
+            }
+        }
+    }
+}
